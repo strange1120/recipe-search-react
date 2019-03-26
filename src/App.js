7f5496564bed8e45c05a63d80925aae5503f803e
Sarah Strange <strange1120@gmail.com>
2019-03-26 15:03:56 -0400

    ajax request done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,29 @@ import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
 class App extends Component {
+  state = {
+    recipes:[],
+    url: "https://www.food2fork.com/api/search?key=abc1fd15e14c625d628c4ced147c84d5"
+  };
+
+  async getRecipes() {
+    try {
+      const data = await fetch(this.state.url);
+    const jsonData = await data.json();
+    this.setState({
+      recipes: jsonData.recipes
+      });
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  componentDidMount() {
+    this.getRecipes()
+  }
+
   render() {
+    console.log(this.state.recipes);
     return (
       <React.Fragment>
         <RecipeList />
